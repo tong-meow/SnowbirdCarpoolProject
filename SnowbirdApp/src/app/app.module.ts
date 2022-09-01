7da34acf9cb020374ea222824f2eb6b5593f5370
tong-meow <tongshen930@gmail.com>
2022-08-31 18:33:40 -0600

    Add routers and user component

--- SnowbirdApp/src/app/app.module.ts
+++ SnowbirdApp/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CarpoolComponent } from './carpool/carpool.component';
 import { CarpoolsComponent } from './carpools/carpools.component';
+import { UserComponent } from './user/user.component';
+
+// routes are stored as an array, each element is a JS object
+const appRoutes: Routes = [
+  // path: what get entered in the url after the domain, e.g. localhost:4200/user
+  { path: '', component: CarpoolsComponent }, // current main page: daily carpools
+  { path: 'user', component: UserComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     CarpoolComponent,
-    CarpoolsComponent
+    CarpoolsComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
