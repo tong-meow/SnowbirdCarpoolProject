58dd4773cc70d5e1bed34faf28431d0b8e56a9be
tong-meow <tongshen930@gmail.com>
2022-09-06 16:55:08 -0600

    Added Firebase authentication

--- SnowbirdApp/src/app/app.module.ts
+++ SnowbirdApp/src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from 'src/environments/environment.prod';
 
 import { AppComponent } from './app.component';
 import { CarpoolComponent } from './carpool/carpool.component';
 import { CarpoolsComponent } from './carpools/carpools.component';
 import { UserComponent } from './user/user.component';
+import { LogInComponent } from './log-in/log-in.component';
 
 // routes are stored as an array, each element is a JS object
 const appRoutes: Routes = [
   // path: what get entered in the url after the domain, e.g. localhost:4200/user
-  { path: '', component: CarpoolsComponent }, // current main page: daily carpools
-  { path: 'user', component: UserComponent }
+  { path: '', pathMatch:'full', redirectTo: 'login' },
+  { path: 'login', component: LogInComponent }, // current main page: daily carpools
+  { path: 'home', component: CarpoolComponent }
 ];
 
 @NgModule({
@@ -19,11 +24,14 @@ const appRoutes: Routes = [
     AppComponent,
     CarpoolComponent,
     CarpoolsComponent,
-    UserComponent
+    UserComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
